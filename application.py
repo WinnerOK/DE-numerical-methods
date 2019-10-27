cf034f25475d6d612e1562e43d0c849d0bd7f6fb
WinnerOK <danman339@gmail.com>
2019-10-27 20:02:15 +0300

    privating attributes and functions

--- application.py
+++ application.py
@@ -14,80 +14,80 @@ EPS = 1e-5
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        self.ui = Ui_MainWindow()
+        self.__ui = Ui_MainWindow()
         self.plotter = PlotCanvas()
-        self.setup_ui()
-        self.graph_checkboxes = {}
+        self.__setup_ui()
+        self.__graph_checkboxes = {}
 
-    def setup_ui(self):
-        self.ui.setupUi(self)
-        self.setup_validators()
-        self.setup_buttons()
-        self.ui.gridLayout.addWidget(self.plotter, 0, 0, 1, 4)
+    def __setup_ui(self):
+        self.__ui.setupUi(self)
+        self.__setup_validators()
+        self.__setup_buttons()
+        self.__ui.gridLayout.addWidget(self.plotter, 0, 0, 1, 4)
 
-    def setup_validators(self):
+    def __setup_validators(self):
         positive_integer = QRegularExpression(r"\d+")
         positive_integer_validator = QRegularExpressionValidator(positive_integer)
 
         rational = QRegularExpression(r"-?\d+\.\d+")
         rational_validator = QRegularExpressionValidator(rational)
 
-        self.ui.x0_Input.setValidator(rational_validator)
-        self.ui.y0_Input.setValidator(rational_validator)
-        self.ui.x_Input.setValidator(rational_validator)
-        self.ui.n_Input.setValidator(positive_integer_validator)
-        self.ui.n0_error_Input.setValidator(positive_integer_validator)
-        self.ui.n_error_Input.setValidator(positive_integer_validator)
+        self.__ui.x0_Input.setValidator(rational_validator)
+        self.__ui.y0_Input.setValidator(rational_validator)
+        self.__ui.x_Input.setValidator(rational_validator)
+        self.__ui.n_Input.setValidator(positive_integer_validator)
+        self.__ui.n0_error_Input.setValidator(positive_integer_validator)
+        self.__ui.n_error_Input.setValidator(positive_integer_validator)
 
-    def setup_buttons(self):
-        self.ui.reset_input_Button.clicked.connect(self.reset_input)
-        self.ui.solutions_Button.clicked.connect(self.draw)
+    def __setup_buttons(self):
+        self.__ui.reset_input_Button.clicked.connect(self.reset_input)
+        self.__ui.solutions_Button.clicked.connect(self.draw)
         # self.ui.error_Button.clicked.connect.(drawing function)
 
     def reset_input(self):
-        self.ui.x0_Input.setText("1")
-        self.ui.y0_Input.setText("2")
-        self.ui.x_Input.setText("6")
-        self.ui.n_Input.setText("100")
-        self.ui.f_Input.setText("(1 + y / x) * log(1 + y / x) + y / x")
-        self.ui.y_exact_Input.setText("x * (exp(log(pow(y_0 / x_0 + 1, x / x_0))) - 1)")
+        self.__ui.x0_Input.setText("1")
+        self.__ui.y0_Input.setText("2")
+        self.__ui.x_Input.setText("6")
+        self.__ui.n_Input.setText("100")
+        self.__ui.f_Input.setText("(1 + y / x) * log(1 + y / x) + y / x")
+        self.__ui.y_exact_Input.setText("x * (exp(log(pow(y_0 / x_0 + 1, x / x_0))) - 1)")
 
-    def add_graph_checkbox(self, method_name: str, color: str):
+    def __add_graph_checkbox(self, method_name: str, color: str):
         tmp = QCheckBox(method_name)
         tmp.setStyleSheet("QCheckBox { color: " + color + "}")
         tmp.setChecked(True)
         tmp.stateChanged.connect(self.update_plot)
-        self.ui.checkbox_Layout.addWidget(tmp)
-        self.graph_checkboxes[method_name] = tmp
+        self.__ui.checkbox_Layout.addWidget(tmp)
+        self.__graph_checkboxes[method_name] = tmp
 
     def clear_graph_checkbox(self):
-        for i in reversed(range(1, self.ui.checkbox_Layout.count())):
-            self.ui.checkbox_Layout.itemAt(i).widget().setParent(None)
-        self.graph_checkboxes.clear()
+        for i in reversed(range(1, self.__ui.checkbox_Layout.count())):
+            self.__ui.checkbox_Layout.itemAt(i).widget().setParent(None)
+        self.__graph_checkboxes.clear()
 
-    def notify_mistake(self, message: str, line_edit: Optional[QLineEdit]):
+    def __notify_mistake(self, message: str, line_edit: Optional[QLineEdit]):
         QMessageBox.warning(self, 'Invalid input', message, QMessageBox.Ok,
                             QMessageBox.Ok)
         if line_edit is not None:
             line_edit.setFocus()
             line_edit.selectAll()
 
-    def validate_input(self, data: dict):
+    def __validate_input(self, data: dict):
         # data = self.get_input()
         if data['graph']['x_0'] > data['graph']['x']:
-            self.notify_mistake("X<sub>0</sub> is greater than X", self.ui.x_Input)
+            self.__notify_mistake("X<sub>0</sub> is greater than X", self.__ui.x_Input)
             return False
 
         if data['error']['n_0'] > data['error']['n']:
-            self.notify_mistake("N<sub>0</sub> is greater than N", self.ui.n_error_Input)
+            self.__notify_mistake("N<sub>0</sub> is greater than N", self.__ui.n_error_Input)
             return False
 
         if data['error']['n_0'] <= 0:
-            self.notify_mistake("N<sub>0</sub> must be greater than 0", self.ui.n0_error_Input)
+            self.__notify_mistake("N<sub>0</sub> must be greater than 0", self.__ui.n0_error_Input)
             return False
 
         if data['graph']['n'] <= 0:
-            self.notify_mistake("N must be greater than 0", self.ui.n_Input)
+            self.__notify_mistake("N must be greater than 0", self.__ui.n_Input)
             return False
 
         x_0 = data['graph']['x_0']
@@ -96,10 +96,10 @@ class MainWindow(QMainWindow):
         try:
             if abs(y_0 - y(x_0)) > EPS:
                 print(y(x_0), y_0)
-                self.notify_mistake("Given exact solution does not correspond to the IVP", self.ui.y_exact_Input)
+                self.__notify_mistake("Given exact solution does not correspond to the IVP", self.__ui.y_exact_Input)
                 return False
         except Exception as e:
-            self.notify_mistake(f"An error occured while parsing the exact solution:\n\"{e}\"", self.ui.y_exact_Input)
+            self.__notify_mistake(f"An error occured while parsing the exact solution:\n\"{e}\"", self.__ui.y_exact_Input)
             return False
 
         return True
@@ -108,32 +108,32 @@ class MainWindow(QMainWindow):
         try:
             inp = {
                 "graph": {
-                    "x_0": float(self.ui.x0_Input.text()),
-                    "y_0": float(self.ui.y0_Input.text()),
-                    "x": float(self.ui.x_Input.text()),
-                    "n": int(self.ui.n_Input.text())
+                    "x_0": float(self.__ui.x0_Input.text()),
+                    "y_0": float(self.__ui.y0_Input.text()),
+                    "x": float(self.__ui.x_Input.text()),
+                    "n": int(self.__ui.n_Input.text())
                 },
 
                 "error": {
-                    "n_0": int(self.ui.n0_error_Input.text()),
-                    "n": int(self.ui.n_error_Input.text())
+                    "n_0": int(self.__ui.n0_error_Input.text()),
+                    "n": int(self.__ui.n_error_Input.text())
                 }
             }
         except ValueError:
-            self.notify_mistake("Some of input fields are empty.", None)
+            self.__notify_mistake("Some of input fields are empty.", None)
             return
         ivp = {
             "x_0": inp['graph']['x_0'],
             "y_0": inp['graph']['y_0'],
         }
-        inp["graph"]["y"] = lambda x: eval(self.ui.y_exact_Input.text(),
+        inp["graph"]["y"] = lambda x: eval(self.__ui.y_exact_Input.text(),
                                            locals().update(ivp)
                                            )
-        inp["graph"]["f"] = lambda x, y: eval(self.ui.f_Input.text(),
+        inp["graph"]["f"] = lambda x, y: eval(self.__ui.f_Input.text(),
                                               locals().update(ivp)
                                               )
 
-        if self.validate_input(inp):
+        if self.__validate_input(inp):
             return inp
 
     @pyqtSlot()
@@ -143,7 +143,7 @@ class MainWindow(QMainWindow):
             return
         self.clear_graph_checkbox()
         for name, color in self.plotter.plot(input_data):
-            self.add_graph_checkbox(name, color)
+            self.__add_graph_checkbox(name, color)
 
     @pyqtSlot()
     def update_plot(self):
